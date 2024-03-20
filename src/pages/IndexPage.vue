8135ee80e23c27ac14a8c1c63dffff1fc253a105
MatheusFern <jogafacilgameplays@gmail.com>
2024-03-20 14:16:52 -0300

    inicial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <div>
+      <div class="text-h2">Teste para </div>
+      <div class="text-h1">CONTROLE</div>
+    </div>
+
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
